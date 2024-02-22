075d877a88c3ca0a015f263ec55815598f45e291
aa506302738 <69311295+aa506302738@users.noreply.github.com>
2024-02-22 14:45:05 +0800

    Update GetAppCookie.js

--- KDLK/Scripts/GetAppCookie.js
+++ KDLK/Scripts/GetAppCookie.js
@@ -2,9 +2,10 @@ const $ = new Tool('凯迪拉克');
 
 try {
     const { url, headers } = $request;
-    const { Cookie, access_token, idpuserid, deviceId, client_id } = headers;
+    const { Cookie, access_token, idpuserid, client_id } = headers;
     const { body } = $response;
     const { data } = JSON.parse(body);
+    const deviceId = "ios";
     if (url.includes('baseInfo')) {
         const phone = data.profileInfo.phone;
         $.setStore('KDLK_APP_HEARDERS', {
