cbcac9da9b2af06538b88886cc271fe1ae525573
aa506302738 <69311295+aa506302738@users.noreply.github.com>
2024-04-12 14:40:41 +0800

    Update DailyTask.js

--- KDLK/Scripts/DailyTask.js
+++ KDLK/Scripts/DailyTask.js
@@ -3,9 +3,7 @@ const $ = new Tool('凯迪拉克');
 const KDLK_APP_COOKIE = $.getStore('KDLK_APP_COOKIE');
 const KDLK_APP_HEARDERS = $.getStore('KDLK_APP_HEARDERS');
 const KDLK_APP_ACCESS_TOKEN = $.getStore('KDLK_APP_ACCESS_TOKEN');
-const  deviceId = KDLK_APP_HEARDERS.uuid;
-const { idpUserId, client_id } = KDLK_APP_HEARDERS;
-
+const { idpUserId,deviceId, client_id } = KDLK_APP_HEARDERS;
 let method = 'POST';
 let baseUrl = 'https://app.sgmlink.com:443/service/mycadillacv3/rest/api';
 let headers = {
